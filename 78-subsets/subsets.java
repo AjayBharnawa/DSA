d9be84f408d39929c1adf84d728c5fa927c651dc
Ajay Bharnawa <bharnawaajay246@gmail.com>
2026-09-11 01:47:51 +0530

    Time: 1 ms (87.05%) | Memory: 44.3 MB (38.27%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    static void solve(int nums[], int idx, List <Integer> output, List <List<Integer>> ans ){
+        if(idx >= nums.length){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+
+        int currVal = nums[idx];
+        //include
+        output.add(currVal);
+        solve(nums, idx+1, output, ans);
+
+        output.remove(output.size()-1);
+
+        //exclude
+        solve(nums, idx+1, output, ans);
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List <List<Integer>> ans = new  ArrayList<>();
+        List <Integer> output = new ArrayList<>();
+        int idx = 0;
+
+        solve(nums, idx, output, ans);
+        return ans;
+    }
+}
